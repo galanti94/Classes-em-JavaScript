0705f9a5524a94db0779d2fa7b6cc6d873a76018
Guilherme Roncaratti Galanti <galanti_imp@hotmail.com>
2023-12-29 19:20:54 -0300

    exemplo de objeto em JS

---
+++ Exemplo.js
@@ -0,0 +1,9 @@
+const pessoa = {
+    nome: 'Guilherme',
+    sobrenome: 'Galanti',
+    idade: 29,
+
+    seApresentar() {
+        alert(`Olá, me chamo ${nome} ${sobrenome}. Tenho ${idade} anos!`);
+    }
+};
